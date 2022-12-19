0c6407c59f78d941fab906ecf092208d28ce9daa
v1nek228 <vanechka.gubkin@inbox.ru>
2022-12-19 10:46:18 +0300

    removed memory leaks

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ int main()
         if (strstr(str + 2, "0x") == NULL)
         {
             printf("ошибка: системы счисления не совпадают\n");
+            free(str);
             return -1;
         }
         int i = check_hex(str);
@@ -30,6 +31,7 @@ int main()
         if (i == -1)
         {
             printf("ошибка: недопустимые символы\n");
+            free(str);
             return -1;
         }
         int a = 10;
@@ -74,6 +76,7 @@ int main()
                     break;
                 default:
                     printf("операция не определена\n");
+                    free(str);
                     return -1;
             }
         }
@@ -105,12 +108,14 @@ int main()
                     break;
                 default:
                     printf("операция не определена\n");
+                    free(str);
                     return -1;
             }
         }
         else
         {
             printf("некорректный ввод (несимметричный)\n");
+            free(str);
             return -1;
         }
         free(src1);
@@ -118,6 +123,7 @@ int main()
         src3 = do_hex(a);
         printf("0x%s (%d)\n", src3, a);
         free(src3);
+        free(str);
         return 0;
     }
     if (str[0] == '0')
@@ -132,12 +138,14 @@ int main()
             strstr(str + 1, "-0") == NULL && strstr(str + 1, "^0") == NULL)
         {
             printf("ошибка: системы счисления не совпадают\n");
+            free(str);
             return -1;
         }
         int i = check_oct(str);   
         if (i == -1)
         {
             printf("ошибка: недопустимые символы\n");
+            free(str);
             return -1;
         }
 
@@ -182,6 +190,7 @@ int main()
                     break;
                 default:
                     printf("операция не определена\n");
+                    free(str);
                     return -1;
             }
         }
@@ -213,12 +222,14 @@ int main()
                     break;
                 default:
                     printf("операция не определена\n");
+                    free(str);
                     return -1;
             }
         }
         else
         {
             printf("некорректный ввод (несимметричный)\n");
+            free(str);
             return -1;
         }
         free(src1);
@@ -226,6 +237,7 @@ int main()
         src3 = do_oct(a);
         printf("0%s (%d)\n", src3, a);
         free(src3);
+        free(str);
         return 0;
     }
     if (str[0] == '~')
@@ -251,15 +263,18 @@ int main()
                 src3 = do_hex(a);
                 printf("0x%s (%d)\n", src3, a);
                 free(src3);
+                free(str);
                 return 0;
             }
-            return 0;
+            printf("ошибка: недопустимые символы\n");
+            free(str);
+            return -1;
         }
         else if (str[1] == '0')
         {
             for (int i = 2; i < strlen(str); i++)
             {
-                if (str[i] < 47 && str[i] > 55)
+                if (str[i] < 47 || str[i] > 55)
                 {
                     f = 1;
                     break;
@@ -273,9 +288,12 @@ int main()
                 src3 = do_oct(a);
                 printf("0%s (%d)\n", src3, a);
                 free(src3);
+                free(str);
                 return 0;
             }
-            return 0;
+            printf("ошибка: недопустимые символы\n");
+            free(str);
+            return -1;
         }
         for (int i = 1; i < strlen(str); i++)
         {
@@ -293,6 +311,7 @@ int main()
             src3 = do_bin(a);
             printf("%s (%d)\n", src3, a);
             free(src3);
+            free(str);
             return 0;
         }
         f = 0;
@@ -310,9 +329,12 @@ int main()
             a = do_dec_dec(str);
             a = ~a;
             printf("%d\n", a);
+            free(str);
             return 0;
         }
-        return 0;
+        printf("ошибка: система счисления не определена\n");
+        free(str);
+        return -1;
             
     }
     
@@ -342,12 +364,14 @@ int main()
             strstr(str + 1, "-1") == NULL && strstr(str + 1, "^1") == NULL)
         {
             printf("ошибка: системы счисления не совпадают\n");
+            free(str);
             return -1;
         }
         int i = check_bin(str);   
         if (i == -1)
         {
             printf("ошибка: недопустимые символы\n");
+            free(str);
             return -1;
         }
 
@@ -392,6 +416,7 @@ int main()
                     break;
                 default:
                     printf("операция не определена\n");
+                    free(str);
                     return -1;
             }
         }
@@ -423,12 +448,14 @@ int main()
                     break;
                 default:
                     printf("операция не определена\n");
+                    free(str);
                     return -1;
             }
         }
         else
         {
             printf("некорректный ввод (несимметричный)\n");
+            free(str);
             return -1;
         }
         free(src1);
@@ -436,6 +463,7 @@ int main()
         src3 = do_bin(a);
         printf("%s (%d)\n", src3, a);
         free(src3);
+        free(str);
         return 0;
     }
     
@@ -451,6 +479,7 @@ int main()
             (str[i + 1] == ' ' ? str[i + 2] < 49 || str[i + 2] > 57 : str[i + 1] < 49 || str[i + 1] > 57))
         {
             printf("ошибка: системы счисления не совпадают\n");
+            free(str);
             return -1;
         }
     }        
@@ -459,6 +488,7 @@ int main()
     if (i == -1)
     {
         printf("ошибка: недопустимые символы\n");
+        free(str);
         return -1;
     }
 
@@ -503,6 +533,7 @@ int main()
                 break;
             default:
                 printf("операция не определена\n");
+                free(str);
                 return -1;
         }
     }
@@ -534,12 +565,14 @@ int main()
                 break;
             default:
                 printf("операция не определена\n");
+                free(str);
                 return -1;
         }
     }
     else
     {
         printf("некорректный ввод (несимметричный)\n");
+        free(str);
         return -1;
     }
     printf("%d\n", a);
